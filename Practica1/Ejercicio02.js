971c89154bbad8b25971486196ed667aed6c0a12
karolshdwst <124049686@upq.edu.mx>
2025-09-18 20:18:49 -0600

    Ejercicio 2 completado
    En este ejercicio transformé la función a una arrow function

---
+++ Practica1/Ejercicio02.js
@@ -0,0 +1,12 @@
+/*function cuadrado(numero) {
+    return numero * numero;
+}
+console.log(cuadrado(5));
+console.log(cuadrado(2));
+console.log(cuadrado(10));*/
+const cuadrado = n => n * n;
+
+console.log(cuadrado(5));
+console.log(cuadrado(2));
+console.log(cuadrado(10));
+
