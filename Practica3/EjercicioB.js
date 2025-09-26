b2f546ff4fca274cfe83b52014377c68c2394e32
karolshdwst <124049686@upq.edu.mx>
2025-09-25 20:08:43 -0600

    Se completó el ejercicio B
    En este ejercicio tuve que crear una función que retorna una promesa, dentro de la promesa tengo un if que verifica si el usuario es igual a "admin" que diga "Acceso concedido", en caso que no sea así, dirá "Acceso denegado".
    Después usé la función pero le pasé como parámetro  "admin" para que me diera acceso concedido y despues "Iván" para que me diera acceso denegado

---
+++ Practica3/EjercicioB.js
@@ -0,0 +1,19 @@
+function verificaUsuario(usuario) {
+    //retorna una promesa aquí
+    return new Promise((resolve, reject) => {
+        if (usuario === "admin") {
+            resolve("Acceso concedido")
+        } else {
+            reject("Acceso denegado")
+        }
+    })
+}
+
+//usa .then() y catch() para manejar el resultado
+verificaUsuario("admin")
+    .then(res => console.log(res))//Acceso concedido
+    .catch(err => console.error(err))
+
+verificaUsuario("Iván")
+    .then(res => console.log(res))
+    .catch(err => console.error(err))//Acceso denegado
