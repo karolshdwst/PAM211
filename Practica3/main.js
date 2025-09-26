9c0166e4550d64e913bc956c77eecaa41d46cf90
karolshdwst <124049686@upq.edu.mx>
2025-09-25 19:45:03 -0600

    Se completó el Ejercicio a
    En este ejercicio se creó un archivo utils.js en el que escribí una función sencilla para restar 2 números y como la voy a usar en el archivo main.js, tuve que escribir la palabra export antes de function. En el archivo main.js importo la función restar desde el archivo utils.js y escribí varios console.log para mostrar el funcionamiento de la función

---
+++ Practica3/main.js
@@ -0,0 +1,6 @@
+import { restar } from './utils.js';
+console.log(restar(5, 3));
+console.log(restar(10, 5));
+console.log(restar(1000, 5432));
+console.log(restar(8, 2));
+console.log(restar(14324122, 12112));
