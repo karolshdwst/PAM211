3d42d20d461cbb5dfefe61f5baa8b7a5bd4ff65e
karolshdwst <124049686@upq.edu.mx>
2025-10-18 22:52:13 -0600

    Actualizacicón Botones

--- INTRO/screens/BotonesScreen.js
+++ INTRO/screens/BotonesScreen.js
@@ -1,12 +1,70 @@
-import { Text, View, StyleSheet } from 'react-native'
+import React, {useState} from 'react'
+import {View, Text, StyleSheet, Touchable, TouchableOpacity, Switch} from 'react-native'
 
 export default function BotonesScreen() {
+    const [prendido, setPrendido] = useState(false);
+    const backgroundColor = prendido ? '#F5F5F5' : '#121212';
+    const textoColor = prendido ? '#000' : '#FFF';
 
     return (
-        <View>
-            <Text>Proximamente....</Text>
-        </View>
+        <View style={[styles.container, {backgroundColor}]}>
+            <Text style={[styles.text, {color: textoColor}]}>Estado: {prendido ? "Prendido" : "Apagado"}</Text>
+            
+            <TouchableOpacity style={styles.botonEncender} onPress={() => setPrendido(true)}>
+                <Text style={styles.textBoton}>Encender</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.botonApagar} onPress={() => setPrendido(false)}>
+                <Text style={styles.textBoton}>Apagar</Text>
+            </TouchableOpacity>
+
+            <View style={styles.switchContainer}>
+                <Text style={[styles.switchLabel, {color: textoColor}]}>Controles Switch: </Text>
+                <Switch value={prendido} onValueChange={setPrendido} />
+                </View>
+            </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#b4b4b4ff',
+    },
+    text : {
+        fontSize: 20,
+        marginBottom: 30,
+        fontWeight: 'bold',
+    },
+    botonEncender: {
+        backgroundColor: '#4CAF50',
+        padding: 10,
+        marginBottom: 20,
+        borderRadius: 5,
+        width: 150,
+        alignItems: 'center',
+    },
+    botonApagar: {
+        backgroundColor: '#ff0000',
+        padding: 10,
+        borderRadius: 5,
+        width: 150,
+        alignItems: 'center',
+    },
+    textBoton: {
+        color: '#ffffffff',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    switchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    switchLabel: {
+        fontSize: 16,
+        marginRight: 10,
+    }
+})
