f1661a903c913d296664f3db2726815400ef030c
shdwst <124049686@upq.edu.mx>
2025-10-27 18:54:28 -0600

    Agregar ScrollViewScreen con contenido y estilos para mejorar la visualización

--- INTRO/screens/ScrollView.js
+++ INTRO/screens/ScrollView.js
@@ -1,12 +1,106 @@
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, StyleSheet, View, ScrollView } from 'react-native'
+import React from 'react'
 
-export default function ScrollView() {
+export default function ScrollViewScreen(){
+        return (
+            <View style={styles.container}>
+                <ScrollView style={styles.scrollArea} contentContainerStyle={styles.scrollContent} showsVerticalScrollIndicator={true}>
+                    
+                    <Text style={styles.title}>Lorem</Text>
+                    <Text style={styles.item}>Aquí va a ir el lorem papus</Text>
+                    <Text style={styles.textRed}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quidem eum ea corrupti dolorum, a esse quis exercitationem rem vitae aut laborum incidunt enim maxime dolor fugit dolorem totam inventore minus iure adipisci aliquid eveniet facilis. Perferendis, illo nobis! Eaque corrupti in magnam molestiae, ut doloribus pariatur, iure sunt sit cumque veniam nemo consequuntur. Error quos ratione ea placeat laboriosam. Eligendi nisi aut harum deserunt. </Text>
 
-    return (
-        <View>
-            <Text>Proximamente....</Text>
-        </View>
-    )
+                    <Text style={styles.title}>Lorem</Text>
+                    <Text style={styles.item}>Aquí va a ir el lorem papus</Text>
+                    <Text style={styles.textRed}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quidem eum ea corrupti dolorum, a esse quis exercitationem rem vitae aut laborum incidunt enim maxime dolor fugit dolorem totam inventore minus iure adipisci aliquid eveniet facilis. Perferendis, illo nobis! Eaque corrupti in magnam molestiae, ut doloribus pariatur, iure sunt sit cumque veniam nemo consequuntur. Error quos ratione ea placeat laboriosam. Eligendi nisi aut harum deserunt. </Text>
+
+                    <Text style={styles.title}>Lorem</Text>
+                    <Text style={styles.item}>Aquí va a ir el lorem papus</Text>
+                    <Text style={styles.textRed}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quidem eum ea corrupti dolorum, a esse quis exercitationem rem vitae aut laborum incidunt enim maxime dolor fugit dolorem totam inventore minus iure adipisci aliquid eveniet facilis. Perferendis, illo nobis! Eaque corrupti in magnam molestiae, ut doloribus pariatur, iure sunt sit cumque veniam nemo consequuntur. Error quos ratione ea placeat laboriosam. Eligendi nisi aut harum deserunt. </Text>
+
+                    <Text style={styles.title}>Lorem</Text>
+                    <Text style={styles.item}>Aquí va a ir el lorem papus</Text>
+                    <Text style={styles.textRed}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quidem eum ea corrupti dolorum, a esse quis exercitationem rem vitae aut laborum incidunt enim maxime dolor fugit dolorem totam inventore minus iure adipisci aliquid eveniet facilis. Perferendis, illo nobis! Eaque corrupti in magnam molestiae, ut doloribus pariatur, iure sunt sit cumque veniam nemo consequuntur. Error quos ratione ea placeat laboriosam. Eligendi nisi aut harum deserunt. </Text>
+                    <Text style={styles.title}>Lorem</Text>
+                    <Text style={styles.item}>Aquí va a ir el lorem papus</Text>
+                    <Text style={styles.textRed}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quidem eum ea corrupti dolorum, a esse quis exercitationem rem vitae aut laborum incidunt enim maxime dolor fugit dolorem totam inventore minus iure adipisci aliquid eveniet facilis. Perferendis, illo nobis! Eaque corrupti in magnam molestiae, ut doloribus pariatur, iure sunt sit cumque veniam nemo consequuntur. Error quos ratione ea placeat laboriosam. Eligendi nisi aut harum deserunt. </Text>
+
+                    <Text style={styles.title}>Lorem</Text>
+                    <Text style={styles.item}>Aquí va a ir el lorem papus</Text>
+                    <Text style={styles.textRed}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quidem eum ea corrupti dolorum, a esse quis exercitationem rem vitae aut laborum incidunt enim maxime dolor fugit dolorem totam inventore minus iure adipisci aliquid eveniet facilis. Perferendis, illo nobis! Eaque corrupti in magnam molestiae, ut doloribus pariatur, iure sunt sit cumque veniam nemo consequuntur. Error quos ratione ea placeat laboriosam. Eligendi nisi aut harum deserunt. </Text>
+
+                    <Text style={styles.title}>Lorem</Text>
+                    <Text style={styles.item}>Aquí va a ir el lorem papus</Text>
+                    <Text style={styles.textRed}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quidem eum ea corrupti dolorum, a esse quis exercitationem rem vitae aut laborum incidunt enim maxime dolor fugit dolorem totam inventore minus iure adipisci aliquid eveniet facilis. Perferendis, illo nobis! Eaque corrupti in magnam molestiae, ut doloribus pariatur, iure sunt sit cumque veniam nemo consequuntur. Error quos ratione ea placeat laboriosam. Eligendi nisi aut harum deserunt. </Text>
+
+                    <Text style={styles.title}>Lorem</Text>
+                    <Text style={styles.item}>Aquí va a ir el lorem papus</Text>
+                    <Text style={styles.textRed}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quidem eum ea corrupti dolorum, a esse quis exercitationem rem vitae aut laborum incidunt enim maxime dolor fugit dolorem totam inventore minus iure adipisci aliquid eveniet facilis. Perferendis, illo nobis! Eaque corrupti in magnam molestiae, ut doloribus pariatur, iure sunt sit cumque veniam nemo consequuntur. Error quos ratione ea placeat laboriosam. Eligendi nisi aut harum deserunt. </Text>
+
+                    <Text style={styles.title}>Lorem</Text>
+                    <Text style={styles.item}>Aquí va a ir el lorem papus</Text>
+                    <Text style={styles.textRed}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quidem eum ea corrupti dolorum, a esse quis exercitationem rem vitae aut laborum incidunt enim maxime dolor fugit dolorem totam inventore minus iure adipisci aliquid eveniet facilis. Perferendis, illo nobis! Eaque corrupti in magnam molestiae, ut doloribus pariatur, iure sunt sit cumque veniam nemo consequuntur. Error quos ratione ea placeat laboriosam. Eligendi nisi aut harum deserunt. </Text>
+                </ScrollView>
+            </View>
+        )
+    
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#a0a0a0ff',
+    },
+    scrollArea: {
+        flex: 1
+    },
+    scrollContent: {
+        alignItems: 'center',
+    },
+    item: {
+        backgroundColor: '#ff0000ff',
+        color: '#ffffffff',
+        marginVertical: 8,
+        padding: 12,
+        borderRadius: 10,
+        textAlign: 'center'
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#FFD700',
+        textAlign: 'center',
+        marginBottom: 20
+    },
+    subtitleRed: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center',
+        backgroundColor: '#ff0000ff',
+        borderRadius: 10,
+        marginVertical: 12,
+        paddingVertical: 12,
+        paddingHorizontal: 10,
+        width: '95%',
+        alignSelf: 'center'
+    },
+    text: {
+        fontSize: 16,
+        color: '#ffffffff',
+        textAlign: 'center',
+        marginBottom: 20,
+        padding: 10,
+        lineHeight: 24,
+    },
+    textRed: {
+        fontSize: 16,
+        color: '#ffffffff',
+        lineHeight: 24,
+        textAlign: 'justify',
+        marginBottom: 15,
+        padding: 10,
+        backgroundColor: '#FF0000',
+        borderRadius: 10,
+    }
+})
