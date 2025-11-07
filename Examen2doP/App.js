dffd8f3ccd3eeb56105484958bf293ff560b00d7
karolshdwst <124049686@upq.edu.mx>
2025-11-06 19:51:01 -0600

    Primer commit del examen

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,20 +1,95 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, ImageBackground, FlatList } from 'react-native';
+import React, {useState, useEffect} from 'react'
+import { Button } from 'react-native-web';
+
+const BackgroundImage = require('./assets/westernBacon.jpg')
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="dark" />
+
+  const platosPrincipales = [
+    {id: '1', nombre: 'Western Bacon', descripcion: 'La mejor burger', precio: '$200',},
+    {id: '2', nombre: 'Western Bacon', descripcion: 'La mejor burger', precio: '$200',},
+    {id: '3', nombre: 'Western Bacon', descripcion: 'La mejor burger', precio: '$200',},
+    {id: '4', nombre: 'Western Bacon', descripcion: 'La mejor burger', precio: '$200',},
+    {id: '5', nombre: 'Western Bacon', descripcion: 'La mejor burger', precio: '$200',}
+  ]
+
+  const renderItem = ({item}) => (
+    <View style={styles.item}>
+      <Text style={styles.titulo}>{item.nombre}</Text>
+      <Text style={styles.descripcion}>{item.descripcion}</Text>
+      <Text style={styles.precio}>{item.precio}</Text>
+      <Button>Picale we a ver que pasa</Button>
     </View>
+  )
+
+  return (
+    <ImageBackground
+      source={BackgroundImage}
+      style={styles.background}
+      resizeMode='cover'
+    >
+      <View style={styles.ViewTituloDescripcion}>
+          <Text style={styles.mainText}>
+            Bienvenido al restaurande Cardenales UPQ
+          </Text>
+          <br/>
+          <Text style={styles.descripcionText}>
+            El mejor restaurante de toda la UPQ
+          </Text>
+        </View>
+        <br/>
+      <ScrollView>
+        <View>
+          <Text style={styles.subtitulo}>
+            Platillos/Bebidas:
+          </Text>
+          <FlatList>
+            
+          </FlatList>
+        </View>
+      </ScrollView>
+
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    alignItems: 'center',
+    width: '100%',
+    height: '100%',
     justifyContent: 'center',
-    padding: 20,
+    alignItems: 'center'
   },
+  ViewTituloDescripcion: {
+        flex: 1,
+        padding: '15px',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    mainText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#000000ff',
+        backgroundColor: '#ffffffff',
+        borderRadius: '5px'
+    },
+    descripcionText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#000000ff',
+        backgroundColor: '#ffffffff',
+        borderRadius: '5px'
+    },
+    subtitulo: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#ffffffff',
+        backgroundColor: '#000000ff',
+        borderRadius: '5px'
+    }
 });
