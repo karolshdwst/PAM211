18478cc37d67c28e23c1a16d73107876e73201c7
karolshdwst <124049686@upq.edu.mx>
2025-11-06 19:06:02 -0600

    Revert "."
    This reverts commit 1924f09a83bdeb9f40bcc6a6b401eee9cd599677.

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ola</Text>
+      <Text>Hello World</Text>
       <StatusBar style="dark" />
     </View>
   );
