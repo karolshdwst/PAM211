33bec62c77d1eae198c7e8996a3f5c2ab59ef3a8
karolshdwst <124049686@upq.edu.mx>
2025-11-13 20:09:48 -0600

    Practica No.15:  Navegation: Stack

---
+++ StackNavigation/screens/settings.js
@@ -0,0 +1,55 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+export default function Settings({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.tittle}>Pantalla de configuracion</Text>
+
+            <Pressable style={[styles.button, styles.buttonProfile]} onPress={() => navigation.navigate('Profile')}>
+                <Text style={styles.buttontext}>Ir a perfil</Text>
+            </Pressable>
+
+            <Pressable style={[styles.button, styles.buttonHome]} onPress={() => navigation.navigate('Home')}>
+                <Text style={styles.buttontext}>Volver a home</Text>
+            </Pressable>
+
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    tittle: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 40,
+        textAlign: 'center',
+    },
+    button: {
+        paddingVertical: 12,
+        paddingHorizontal: 30,
+        borderRadius: 8,
+        marginBottom: 20,
+        width: '80%',
+        alignItems: 'center',
+    },
+    buttonProfile: {
+        backgroundColor: '#007BFF',
+    },
+    buttonSettings: {
+        backgroundColor: '#FF8800',
+    },
+    buttonHome: {
+        backgroundColor: '#28A745',
+    },
+    buttontext: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '600',
+    }
+});
